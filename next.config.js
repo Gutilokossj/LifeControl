33c9d279241b669ba68da14fe9eef7041602c9ca
Gutilokossj <gutilokossj12@gmail.com>
2025-04-28 15:35:16 -0300

    Adicionado ipv4 nas configs dev do package.json

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    devIndicators: {
+      autoPrerender: false,
+    },
+    serverRuntimeConfig: {},
+    publicRuntimeConfig: {},
+  };
 
 module.exports = nextConfig;
