0de64058e1c2ebcb4d3802fd4953eb1c96240e10
Gutilokossj <gutilokossj12@gmail.com>
2025-05-23 12:55:25 -0300

    Melhoria simples na estilização!

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,31 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col p-10 min-h-screen">
-      <h1 className="text-3xl font-bold">Home</h1>
+      <h1 className="text-3xl font-bold">Seja bem-vindo(a)</h1>
 
-      <div className="font-semibold text-2xl grid grid-cols-1 gap-5 sm:grid-cols-2">
-      <Link href="/tasks">
-        <p className="bg-blue-300 justify-center items-center text-center p-5 mt-10 cursor-pointer rounded-md hover:translate-x-1 duration-300">
-          Controle de tarefas</p>
-      </Link>
-      <p className="bg-green-600 justify-center items-center text-center p-5 mt-10 cursor-pointer rounded-md hover:translate-x-1 duration-300">
-        Controle de financeiro</p> 
-        <p className="bg-yellow-500 justify-center items-center text-center p-5 mt-10 cursor-pointer rounded-md hover:translate-x-1 duration-300">
-        Controle de estudos</p>
-        <p className="bg-orange-400 justify-center items-center text-center p-5 mt-10 cursor-pointer rounded-md hover:translate-x-1 duration-300">
-        Controle de treinos</p>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 mt-10">
+        <Link href="/tasks">
+          <div className="bg-blue-500 text-white text-center p-6 rounded-2xl shadow-md cursor-pointer hover:scale-105 transition-transform duration-300">
+            <p className="text-lg sm:text-xl font-semibold">
+              Controle de Tarefas
+            </p>
+          </div>
+        </Link>
+        <div className="bg-green-500 text-white text-center p-6 rounded-2xl shadow-md cursor-pointer hover:scale-105 transition-transform duration-300">
+          <p className="text-lg sm:text-xl font-semibold">
+            Controle Financeiro
+          </p>
+        </div>
+        <div className="bg-yellow-400 text-black text-center p-6 rounded-2xl shadow-md cursor-pointer hover:scale-105 transition-transform duration-300">
+          <p className="text-lg sm:text-xl font-semibold">
+            Controle de Estudos
+          </p>
+        </div>
+        <div className="bg-orange-500 text-white text-center p-6 rounded-2xl shadow-md cursor-pointer hover:scale-105 transition-transform duration-300">
+          <p className="text-lg sm:text-xl font-semibold">
+            Controle de Treinos
+          </p>
+        </div>
       </div>
     </div>
   );
